5b8aeb24e4733bf56f88427eb012c8726e566255
Polina <36280649+rabarbra@users.noreply.github.com>
2023-11-17 09:46:56 +0100

    Psimonen (#4)
    * loop() in progress

--- Server.hpp
+++ Server.hpp
@@ -1,13 +1,22 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 # include <map>
+# include <vector>
 # include <string>
 # include "Route.hpp"
 class Server
 {
 	private:
-		std::map<std::string, Route> routes;
+		std::map<std::string, Route>	routes;
+		std::string						host;
+		std::string						port;
+		std::vector<std::string>		server_names;
+		int								max_body_size;
 	public:
+		Server();
+		~Server();
+		Server(const Server &other);
+		Server &operator=(const Server &other);
 		void	setRoute(std::string path, Route &route);
 };
 #endif
